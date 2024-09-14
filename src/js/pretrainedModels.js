d074a59464969c3a09d13ab2c65ac9ca758ce1f9
Alexandros Oikonomou <alexoik2001@gmail.com>
2024-09-14 19:40:41 +0300

    Created FrontEnd Auth Pages (Log In, Sign Up & Forgot Password): • Auth HTML pages implementation: login.html, signUp.html and forgotPassword.html       > login.html: For the users' log in to their existing account.       > signUp.html: For the users' sign up to a new account.       > forgotPassword.html: For the users' reseting password through email. • Common CSS file creation for the UI styling of auth pages: auth.css

--- src/js/pretrainedModels.js
+++ src/js/pretrainedModels.js
@@ -366,19 +366,13 @@ $(function () {
                                     role="button"
                                     download="Decision_Tree_${classifierType}_Label_${index + 1}.png" 
                                     class="btn btn-success d-block mx-auto mb-3"
-                                    style="width: 300px;"
+                                    style="width: 315px;"
                                 >
                                     <svg xmlns="http://www.w3.org/2000/svg" style="width: 22px; margin-bottom: 1px;" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
                                     </svg>
                                     Download
                                 </a>
-                                ${index < images.length - 1 ? `
-                                    <hr 
-                                        class="d-block mx-auto mb-3"
-                                        style="width: 500px;"
-                                    />` : ''
-                                }
                             `);
                         });
                     }
