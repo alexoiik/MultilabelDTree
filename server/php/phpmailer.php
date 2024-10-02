ae7e58346a716bf41d2849e0b52ccc53c5f03b54
Alexandros Oikonomou <alexoik2001@gmail.com>
2024-10-02 21:16:43 +0300

    Fixed Mail Auth Error - "SMTP: can not authenticate".

--- server/php/phpmailer.php
+++ server/php/phpmailer.php
@@ -1,46 +1,52 @@
 <?php
     
     use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
 
     require __DIR__.'/../../vendor/autoload.php';
     require 'mailer_config.php';
 
-    function send_mail($recipient, $r_name, $subject, $body, $altbody){
+    function send_mail($recipient, $r_name, $subject, $body, $altbody) {
 
         require 'mailer_config.php';
         $mail = new PHPMailer(true);
 
         try {
+            // SMTP SetUp.
             $mail->SMTPDebug = 0;       
             $mail->isSMTP();            
-            $mail->Host       = 'smtp-mail.outlook.com';   
-            $mail->SMTPAuth   = true;                      
-            $mail->SMTPOptions=array('ssl'=>array(
-                'verify_peer'=>false,
-                'verify_peer_name'=>false,
-                'allow_self_signed'=>true
-            ));
+            $mail->Host       = 'smtp.zoho.eu';   
+            $mail->SMTPAuth   = true;       
+                           
+            // SMTP Options.
+            $mail->SMTPOptions=array(
+                'ssl'=>array(
+                    'verify_peer'=>false,
+                    'verify_peer_name'=>false,
+                    'allow_self_signed'=>true
+                )
+            );
             
+            // Email Credentials & SMTP Info.
             $mail->Username   = $username;     
             $mail->Password   = $password;     
             $mail->SMTPSecure = 'tls';         
             $mail->Port       = 587;    
 
+            // Email SetUp.
             $mail->setFrom($username, 'MultilabelDTree App');
             $mail->addAddress($recipient, $r_name);  
-        
             $mail->isHTML(true);                     
             $mail->Subject = $subject;
             $mail->Body    = $body;
             $mail->AltBody = $altbody;
 
+            // Sending Email.
             $mail->send();
 
         } catch (Exception $e) {
             header("HTTP/1.1 400 Bad Request");
-            print json_encode(['errormesg'=>"Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
+            print json_encode(['errormesg'=>"Mailer Error: {$mail->ErrorInfo}"]);
             exit;
         }
     }
